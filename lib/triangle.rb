a3be18017225f177845fe868b558310ac6f1feec
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-06 11:57:36 -0500

    commit tree 8a46a9dff426fd079cb1b10cbf7d642ab851a6dd

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/PerceivedComplexity
+
+  def kind
+    test_arr = [@a, @b, @c]
+    if test_arr.any? { |side| side <= 0 } || ((@a + @b <= @c) || (@b + @c <= @a) || (@a + @c <= @b))
+      raise TriangleError
+    elsif test_arr.all? { |side| side == @a }
+      :equilateral
+    elsif (@a != @b && @b == @c) || (@a != @b && @c == @a) || (@a == @b && @b != @c)
+      :isosceles
+    elsif test_arr.uniq
+      :scalene
+    end
+  end
+
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/PerceivedComplexity
+
+  class TriangleError < StandardError
+    def message
+      'These are not triangles!'
+    end
+  end
 end
